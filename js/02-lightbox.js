845693c661d4c1800f30eba9273932210cb5e35b
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2022-12-12 22:27:44 +0200

    + second

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery=document.querySelector('.gallery');
+const arrayOfPictures=createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML('beforeend',arrayOfPictures);
+
+function createGalleryMarkup(array){
+return array.map(({preview,original,description})=>
+`
+<div class="gallery__item">
+
+<a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`).join('');
+
+}
+
+let lightbox = new SimpleLightbox('.gallery a', );
