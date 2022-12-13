e4e84e3ba195d39c4e6bb5cee59652f016ef39f3
YuliiaVlasiuk <vlasyukyulia@gmail.com>
2022-12-14 00:05:08 +0200

    2 tast add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,15 +1,17 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 let originalGallery;
 
-const gallery=document.querySelector('.gallery');
-const arrayOfDiv=createGalleryMarkup(galleryItems);
-gallery.insertAdjacentHTML('beforeend',arrayOfDiv);
+const gallery = document.querySelector(".gallery");
+const arrayOfDiv = createGalleryMarkup(galleryItems);
+gallery.insertAdjacentHTML("beforeend", arrayOfDiv);
 
-function createGalleryMarkup(array){
-return array.map(({preview,original,description})=>
-`
+function createGalleryMarkup(array) {
+  return array
+    .map(
+      ({ preview, original, description }) =>
+        `
 <div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
@@ -19,50 +21,34 @@ return array.map(({preview,original,description})=>
       alt="${description}"
     />
   </a>
-</div>`).join('');
-
+</div>`
+    )
+    .join("");
 }
 
+gallery.addEventListener("click", openOriginalGallery);
 
-gallery.addEventListener('click',openOriginalGallery);
+function openOriginalGallery(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
 
-function openOriginalGallery(evt){
-evt.preventDefault();
-
-if (!evt.target.classList.contains('gallery__image') ){
-     return
-}
-
-originalGallery = basicLightbox.create(`
+  originalGallery = basicLightbox.create(`
 	<img
     class="gallery__image"
     src="${evt.target.dataset.source}"
      alt="${evt.target.description}"
-  />
-`)
+  />`);
 
-originalGallery.show();
-document.addEventListener("keydown", closeOriginalGalleryEscape);
+  originalGallery.show();
+
+  document.addEventListener("keydown", closeOriginalGalleryEscape);
 }
 
 function closeOriginalGalleryEscape(evt) {
-if (evt.code === "Escape") {
-  originalGallery.close();
-  documents.removeEventListener("keydown", closeOriginalGalleryEscape);
+  if (evt.code === "Escape") {
+    originalGallery.close();
+    documents.removeEventListener("keydown", closeOriginalGalleryEscape);
+  }
 }
-}
-
-
-// let counter=0;
-
-// document.addEventListener('scroll', onScroll)
-
-// function onScroll(evt){
-//     console.log(evt);
-//     counter +=1;
-//     console.log(counter);
-// }
-
-
-
-
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,14 +9,12 @@ gallery.insertAdjacentHTML('beforeend',arrayOfPictures);
 
 function createGalleryMarkup(array){
 return array.map(({preview,original,description})=>
-`
-<div class="gallery__item">
-
-<a class="gallery__link" href="${original}">
+`<a class="gallery__item" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
-</a>
-</div>`).join('');
+</a>`).join('');
 
 }
-
-let lightbox = new SimpleLightbox('.gallery a', );
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
